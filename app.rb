a15878173d2ac8159ead626894f917e44279fa73
jimbolit <james.little@kellogg.northwestern.edu>
2020-03-23 22:17:00 +0000

    Initial setup

--- app.rb
+++ app.rb
@@ -1,1 +1,103 @@
+require "sinatra"                                                               
+require "sinatra/reloader" if development?                                      
+require "sequel"                                                                
+require "logger"                                                                
+require "twilio-ruby"                                                           
+require "bcrypt"   
+require "geocoder"                                                             
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB ||= Sequel.connect(connection_string)                                        
+DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                    
+def view(template); erb template.to_sym; end                                    
+use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'     
+before { puts; puts "--------------- NEW REQUEST ---------------"; puts }       
+after { puts; }                                                                 
+#######################################################################################
 
+lists_table = DB.from(:purchases)
+users_table = DB.from(:users)
+
+# Twilio API credentials and connection
+    account_sid = ENV["twilio_sid"]
+    auth_token = ENV["twilio_token"]
+    client = Twilio::REST::Client.new(account_sid, auth_token)
+
+# Code starts
+
+before do
+    @current_user = users_table.where(id: session["user_id"]).to_a[0]
+end
+
+
+get "/" do
+    @lists = lists_table.all.to_a
+    view "ists"
+end
+
+post "/lists/thanks" do
+        
+    lists_table.insert(user_id: session["user_id"],
+                           items: params["items"],
+                           date_posted: params["date_posted"],
+                           comments:params["comments"],
+                           delivery_location: params["delivery_location"])
+    view "lists_thanks"
+end
+
+get "/list/:id" do
+    @list = purchases_list.where(id: params["id"]).to_a[0]
+    @users_table = users_table
+
+    @location = @purchase[:delivery_location]
+    @results = Geocoder.search("#{@location}")
+    lat_long = @results.first.coordinates
+    lat = lat_long[0]
+    long = lat_long[1]
+    @coords = "#{lat},#{long}"
+
+    view "list"
+end
+
+ 
+# send the SMS from your trial Twilio number to your verified non-Twilio number 
+    client.messages.create(
+    from: "+12076186686", 
+    to: "+642102377971",
+    body: "Thanks for joining the #{@purchase[:title]} bandwagon. This text confirms that you are on the bandwagon!"
+    )
+  
+    view "bandwagon_thanks"
+end
+
+get "/users/new" do
+     view "new_user"
+end
+
+post "/users/create" do
+    puts params
+    hashed_password = BCrypt::Password.create(params["password"])
+    users_table.insert(name: params["name"], email: params["email"], mobile_number: params["mobile_number"],password: hashed_password, mobile_number: params["mobile_number"])
+    view "create_user"
+end
+
+get "/logins/new" do
+    view "new_login"
+end
+
+post "/logins/create" do
+    user = users_table.where(email: params["email"]).to_a[0]
+    puts BCrypt::Password::new(user[:password])
+    if user && BCrypt::Password::new(user[:password]) == params["password"]
+        session["user_id"] = user[:id]
+        @current_user = user
+        view "create_login"
+    else
+        view "create_login_failed"
+    end
+end
+
+get "/logout" do
+    session["user_id"] = nil
+    @current_user = nil
+    view "logout"
+end
---
+++ createdb.rb
@@ -0,0 +1,59 @@
+# Set up for the application and database. DO NOT CHANGE. #############################
+require "sequel"
+require "bcrypt"                                                                      #
+connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
+DB = Sequel.connect(connection_string)                                                #
+#######################################################################################
+
+# Database schema - this should reflect your domain model
+DB.create_table! :lists do
+  primary_key :id
+  foreign_key :user_id
+  String :date_posted
+  String :comments, text: true
+  String :items, text:true
+  String :delivery_location
+end
+DB.create_table! :users do
+  primary_key :id
+  String :name
+  String :email
+  String :password
+  Integer :mobile_number
+end
+
+
+# Insert initial (seed) data
+lists_table = DB.from(:lists)
+users_table = DB.from(:users)
+
+lists_table.insert( 
+                    date_posted: "March 25 2020",
+                    comments: "Please call once you are out the front",
+                    delivery_location: "111 Puka Crescent, Matarangi, New Zealand",
+                    items: "2x Fly spray
+                            Two litre bottle of dark blue milk
+                            1kg bag of plain flour
+                            2x large blocks of black forest chocolate
+                            1kg block of Edam cheese
+                            Bag of ground plunger coffee",
+                    user_id: 1)
+
+purchases_table.insert(
+                    date_posted: "March 26 2020",
+                    comments: "Big house with the green fence",
+                    delivery_location: "6 Westbourne Rd, Remuera, Auckland, New Zealand",
+                    items: "2x 6 pack of two ply toilet paper
+                            1l bottle of pulp orange juice
+                            spf 50 sunscreen 200 ml",
+                    user_id: 1)
+
+users_table.insert(name: "Jim Little", 
+                    email: "jameslittle@outlook.co.nz",
+                    mobile_number: "02102377971",
+                    password: BCrypt::Password.create("jim"))
+
+users_table.insert(name: "Xindi", 
+                    email: "xindi.k.zhang@gmail.com",
+                    mobile_number: "+61419763177"
+                    password: BCrypt::Password.create("xindi"))
